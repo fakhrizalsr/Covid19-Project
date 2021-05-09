f167d9aaa8d724104b118f9eb1fd44acfaf6df91
Fakhrizal Ramadhan <45162464+fakhrizalsr@users.noreply.github.com>
2021-05-10 02:58:10 +0700

    small changes

--- vaksin.php
+++ vaksin.php
@@ -30,7 +30,7 @@ $data = json_decode($json, TRUE);
     <!-- Navbar -->
     <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white">
         <div class="container-fluid">
-            <a class="navbar-brand" href="/Covid19-Project">
+            <a class="navbar-brand" href="/cov19project.herokuapp.com">
                 <img src="assets/img/covid19project.png" alt="" width="165" height="30">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +39,7 @@ $data = json_decode($json, TRUE);
             <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="/Covid19-Project">Home</a>
+                        <a class="nav-link" href="/cov19project.herokuapp.com">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="vaksin.php"><b>Data Vaksin</b></a>
